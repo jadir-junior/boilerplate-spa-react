4ca0ce06bbc0b687a5f8ebe5435b84b8aac1a9c7
Jadir Junior <jadirjsjunior@gmail.com>
2020-11-12 16:15:20 -0300

    config storybook

---
+++ src/App.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import App from './App'
+
+export default {
+  title: 'App',
+  component: App
+} as Meta
+
+export const Template: Story = (args) => <App {...args} />
