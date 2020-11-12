b414f3304b7046179fc0d6b39a9caab45f09f6ec
Jadir Junior <jadirjsjunior@gmail.com>
2020-11-12 15:03:46 -0300

    add rtl config and first test

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnviroment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts']
-}
